5d1dd51afc560bfd2ba270ceaac032f1f9541f28
Michael Anderson <michanders@msn.com>
2018-01-02 22:13:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		response = Rack::Response.new
+		response.status = 200
+		erb :index
 	end
+
+	get '/info' do
+		response = Rack::Response.new
+		response.status = 200
+		erb :info
+  end
 end
