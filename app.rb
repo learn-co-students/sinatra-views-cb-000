84d2429847e098fb63d05d9fb38125b554c60e0b
Jen Poole <contact.vandepool@gmail.com>
2018-05-11 23:30:02 +0000

    index view & controller

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	# update our controller to render the index.erb file at the "/" route
 	get '/' do
-		
+		erb :index
 	end
+
 end
